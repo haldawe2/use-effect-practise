cc0ed8796d32115108c4df0986037a9c7f21fa02
Gerard Guasch <haldawe@hotmail.com>
2023-03-04 12:56:19 +0100

    done

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -6,16 +6,44 @@ export default function Counter() {
   const [dynamicClass, setDynamicClass] = useState('text');
 
   // First iteration
+  const handleIncrease = () => {
+    setCount(prev => prev + 1);
+  };
+
+  const handleDecrease = () => {
+    setCount(prev => prev - 1);
+  };
 
   // Second and Fourth iteration
 
+  useEffect(() => {
+    const randomCount = Math.floor(Math.random()*11);
+    setCount(randomCount);
+
+    return () => {
+      setCount(0);
+      setDynamicClass('text');
+      console.log('All clean here. Count:', count, 'Class:', dynamicClass);
+    }
+  }, []);
+
   // Third iteration
+
+  useEffect(() => {
+    if(count > 5) {
+      setDynamicClass('green');
+    } else if (count >= 0){
+      setDynamicClass('orange');
+    } else {
+      setDynamicClass('red');
+    }
+  }, [count])
 
   return (
     <div>
       <p>Counter: <span className={dynamicClass}>{count}</span></p>
-      <button>+ Increase</button>
-      <button>- Decrease</button>
+      <button onClick={handleIncrease}>+ Increase</button>
+      <button onClick={handleDecrease}>- Decrease</button>
     </div>
   )
 }
